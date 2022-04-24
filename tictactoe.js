f0d139acf2c9cbe6da31e7c44f29579c2274ad2b
Revolution5 <damonboone99@gmail.com>
2022-04-24 13:58:32 -0700

    Display gameboard on the page

---
+++ tictactoe.js
@@ -0,0 +1,13 @@
+const gameBoard = (function() {
+    gameBoardArray = [];
+})();
+
+const displayController = (function() {
+
+})();
+
+const Player = (name) => {
+    return {name};
+}
+
+const playerOne = Player("Damon");
