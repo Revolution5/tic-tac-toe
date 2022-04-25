8428f5f5f37c2d5fc2d8ab6ba9f407848437a5f1
Revolution5 <damonboone99@gmail.com>
2022-04-24 17:24:59 -0700

    Fill gameboard from array

--- tictactoe.js
+++ tictactoe.js
@@ -1,13 +1,30 @@
 const gameBoard = (function() {
-    gameBoardArray = [];
+    let gameBoardArray = ["","X","","X","O","X","","X",""];
+    return {gameBoardArray};
 })();
 
 const displayController = (function() {
-
+    function displayBoard() {
+        for(let i = 0; i < gameBoard.gameBoardArray.length; i++) {
+            //converts the iterator to a string for the square selector
+            //ex: i = 0 will select square0
+            document.querySelector(".square" + i.toString()).textContent = gameBoard.gameBoardArray[i];
+        }
+    }
+    return {displayBoard}
 })();
 
-const Player = (name) => {
-    return {name};
+const Player = (name, mark) => {
+    return {name, mark};
 }
 
-const playerOne = Player("Damon");
+const playerOne = Player("Damon", "X");
+const playerTwo = Player("Sara", "O");
+
+displayController.displayBoard();
+
+//loop thru square divs 
+//if the clicked square's text content is empty
+//add click event that updates the position in the array corresponding
+//with the clicked square with the current players player.mark
+//redisplay the board
