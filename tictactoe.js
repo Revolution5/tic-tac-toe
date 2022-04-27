e248b338e2b68af171067ac7aa0dc436ccc29f7f
Revolution5 <damonboone99@gmail.com>
2022-04-27 14:07:22 -0700

    Add function to determine winner

--- tictactoe.js
+++ tictactoe.js
@@ -1,7 +1,84 @@
 const gameBoard = (function() {
     const gameBoardArray = ["","","","","","","","",""];
     let totalTurns = 0;
-    return {gameBoardArray, totalTurns};
+    const winConditions = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+    let squares = Array.from(document.querySelectorAll(".game-container > div"));
+    
+    function resetBoard() {
+        gameBoardArray = ["","","","","","","","",""];
+    }
+
+    function activateBoard() {
+        squares.forEach(square => {
+            square.addEventListener("click", clickHandler);
+        })
+    }
+    
+    const container = document.querySelector(".global-container");
+    const winnerText = document.createElement("h1");
+    let winner = false;
+
+    function checkForWin() {
+        for(let i = 0; i < winConditions.length; i++){
+            if (gameBoardArray[winConditions[i][0]] == "X" && 
+                gameBoardArray[winConditions[i][1]] == "X" && 
+                gameBoardArray[winConditions[i][2]] == "X") {
+                    winner = true;
+                    winnerText.textContent = "X wins!";
+                    container.prepend(winnerText);
+                    squares.forEach(square => {
+                        square.removeEventListener("click", clickHandler);
+                    })
+            }
+            else if(gameBoardArray[winConditions[i][0]] == "O" && 
+                    gameBoardArray[winConditions[i][1]] == "O" && 
+                    gameBoardArray[winConditions[i][2]] == "O") {
+                        winner = true;
+                        winnerText.textContent = "O wins!";
+                        container.prepend(winnerText);
+                        squares.forEach(square => {
+                            square.removeEventListener("click", clickHandler);
+                        })
+            }
+            else if(gameBoardArray.every((element) => element.length > 0) && winner == false) {
+                winnerText.textContent = "Its a tie!";
+                container.prepend(winnerText);
+                squares.forEach(square => {
+                    square.removeEventListener("click", clickHandler);
+                }) 
+            }
+        }
+    }
+
+    function clickHandler(e) {
+        let clicked = e.target.id; //id == square0, square1, etc
+        let number = clicked[6];
+        //uses the number in the id as the array index
+        if(gameBoardArray[number] == "") {
+            if(totalTurns % 2 == 0){
+                gameBoardArray[number] = playerOne.mark;
+                totalTurns++; 
+                checkForWin();
+            }
+            else if(totalTurns % 2 == 1){
+                gameBoardArray[number] = playerTwo.mark;
+                totalTurns++; 
+                checkForWin();
+            }
+            displayController.displayBoard();   
+        }
+    }
+
+    return {gameBoardArray, activateBoard, resetBoard};
 })();
 
 const displayController = (function() {
@@ -22,26 +99,4 @@ const Player = (name, mark) => {
 const playerOne = Player("Damon", "X");
 const playerTwo = Player("Sara", "O");
 
-
-function handler(e) {
-    let clicked = e.target.id; //id == square0, square1, etc
-    let number = clicked[6];
-    //uses the number in the id as the array index
-    if(gameBoard.gameBoardArray[number] == "") {
-        if(gameBoard.totalTurns % 2 == 0){
-           gameBoard.gameBoardArray[number] = playerOne.mark;
-           gameBoard.totalTurns++; 
-        }
-        else if(gameBoard.totalTurns %2 == 1){
-            gameBoard.gameBoardArray[number] = playerTwo.mark; 
-            gameBoard.totalTurns++; 
-        }
-        displayController.displayBoard();   
-     }
-}
-
-let squares = Array.from(document.querySelectorAll(".game-container > div"));
-squares.forEach(square => {
-    square.removeEventListener("click", handler);
-    square.addEventListener("click", handler), {once:true};
-})
+gameBoard.activateBoard();
